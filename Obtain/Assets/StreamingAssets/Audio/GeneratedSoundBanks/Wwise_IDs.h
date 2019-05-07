eaa3f12c942057590afaeefc65e6d76ba6c91b95
Couchpirate <chuu@live.se>
2019-05-07 17:15:24 +0200

    it dooooesnt woooooork yeehaw

---
+++ Obtain/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,70 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID INITIALIZE = 1698951735U;
+        static const AkUniqueID SET_STATE = 2596512161U;
+        static const AkUniqueID ZONE_TRIGGER = 3500303274U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace ZONES
+        {
+            static const AkUniqueID GROUP = 831766718U;
+
+            namespace STATE
+            {
+                static const AkUniqueID DEFAULT = 782826392U;
+                static const AkUniqueID RED_1 = 3046471834U;
+                static const AkUniqueID RED_2 = 3046471833U;
+                static const AkUniqueID RED_3 = 3046471832U;
+            } // namespace STATE
+        } // namespace ZONES
+
+    } // namespace STATES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID DESERT_WEIGHT = 260695189U;
+        static const AkUniqueID QUARRY_WEIGHT = 791424932U;
+        static const AkUniqueID VILLAGE_WEIGHT = 926078990U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID ZONE = 832057375U;
+    } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID SPATIAL = 378350335U;
+    } // namespace AUX_BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
